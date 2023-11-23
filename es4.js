2fea93596e136c30cab578549a2bfcb4b288be46
alessio <mrbigalessio@gmail.com>
2023-11-23 22:37:04 +0100

    lista della spesa 1

---
+++ es4.js
@@ -0,0 +1,41 @@
+//MOSTRARE UNA LISTA//
+
+let listaprodotto = [
+    {
+       
+        prodotto: "pane",
+        
+    },
+    {
+        
+        prodotto: "latte",
+        
+    },
+
+    {
+        
+        prodotto: "salame",
+       
+    },
+    {
+        
+        prodotto: "zucchero",
+        
+    },
+]
+
+
+
+console.log(listaprodotto);
+
+//AGGIUNGERE UN PRODOTTO//
+
+listaprodotto.push("prodotto:" + "banane");
+
+console.log(listaprodotto);
+
+//ORDINARE IN ORDINE ALFABETTICO//
+
+listaprodotto.sort
+
+console.log(listaprodotto);
